aecfa98fcfb6978dde8f35c7c8c7a00cea541b53
Praveen Tirupattur <ptirupat@users.noreply.github.com>
2019-07-09 05:09:05 -0400

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,10 +1,10 @@
 dataset_folder = '~/datasets/UCF_Crime/Videos'
 
-annotations_folder = '~/UCFCrime2Local/annotations'
+annotations_folder = './data/UCFCrime2Local/annotations'
 
-train_split_file = '~/UCFCrime2Local/Train_split_AD.txt'
+train_split_file = './data/UCFCrime2Local/Train_split_AD.txt'
 
-test_split_file = '~/UCFCrime2Local/Test_split_AD.txt'
+test_split_file = './data/UCFCrime2Local/Test_split_AD.txt'
 
 saved_models_dir = './results/saved_models'
 
